94336d12a3a2ac2597a5f7d1eadbfcae4c5ab84b
Abir <abbasa@uoguelph.ca>
2020-01-20 13:01:53 -0500

    added required libraries and permissions

--- app/src/main/java/com/example/cis4030/MainActivity.java
+++ app/src/main/java/com/example/cis4030/MainActivity.java
@@ -3,15 +3,32 @@ package com.example.cis4030;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView tvMessage;
+
+    /* SET PASSWORD HERE PLEASE */
+    public static final String PASSWORD = "SET PASSWORD HERE";
 
     public String getHash(String password) {
         MessageDigest digest=null;
@@ -32,11 +49,16 @@ public class MainActivity extends AppCompatActivity {
     //The goal of the exercise is to call the post request
     //body = { password : hash }
     //response = { status : 200, message : "Secret message" }
-    public String postPromoCode(String password) {
+    public void postPromoCode(String password) throws JSONException {
 
-        //call post API here and return the message within this
+        String url = "https://mobile-computing-weather-app.herokuapp.com/api/store/fungi/promo";
+        RequestQueue queue = Volley.newRequestQueue(this);
 
-        return "Replace this message";
+        //Move this into the API call and update with the message field
+        tvMessage.setText("Replace this with the secret message");
+
+        //uncomment this to add API call to
+        //queue.add(objRequest);
     }
 
 
@@ -44,7 +66,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tvMessage = (TextView)findViewById(R.id.tvMessage);
-        tvMessage.setText(postPromoCode(getHash("mushroompromo")));
+        tvMessage = findViewById(R.id.tvMessage);
+        try {
+            postPromoCode(getHash(PASSWORD));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
